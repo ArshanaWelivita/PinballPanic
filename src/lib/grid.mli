dbec0c9f597407a244da5173a916e7a6d4c2e3f2
Kenneth Elsman <98336240+Kenneth-E@users.noreply.github.com>
2024-11-10 16:39:59 -0500

    updated grid.mli file

--- src/lib/grid.mli
+++ src/lib/grid.mli
@@ -40,15 +40,33 @@ Make a functor from grid module type (all the way at the end - not priority righ
 --> for future, may need ball type to show the visual movement of the ball through the grid
 
 *)
+open Core
+open Bumper
 
 (* Define a position as a tuple of integers *)
 type pos = int * int
 
 val compare_pos : pos -> pos -> bool
 
+val compare_orientation : orientation -> orientation -> bool
+
+val get_level_settings : int -> int * int * int
+
+(* Function to fetch the grid size given the current level
+   Returns grid size n for a square grid *)
+val get_grid_size : int -> int
+
+val out_of_bounds_check : pos -> int -> bool
+
+val move : pos -> direction -> pos
+
+val place_initial_bumper : int array array -> pos -> direction -> int -> pos * orientation
+
+val orientation_for_direction : direction -> orientation
+
+val simulate_ball_path : int array array -> pos -> direction -> int -> int -> orientation -> (pos * direction) Set.Poly.t -> int -> pos
+
 (* Function to generate a grid for a given level
    Returns a matrix (2D array) representing the grid with bumpers 
    and the exit position of the ball *)
 val generate_grid : int -> int array array * pos * pos * Bumper.direction
-
-val get_grid_size : int -> int
