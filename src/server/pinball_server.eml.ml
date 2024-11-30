cbf76895627aa2a28117c1820204d08ea5e1d646
Kenneth Elsman <98336240+Kenneth-E@users.noreply.github.com>
2024-11-30 13:49:20 -0500

    test page is running

--- src/server/pinball_server.eml.ml
+++ src/server/pinball_server.eml.ml
@@ -1,5 +1,6 @@
 open Core
 open Grid_cell
+(* open Lwt.Syntax *)
 
 (* Game state information *)
 let current_level = ref 1
@@ -15,7 +16,7 @@ let render_grid (grid : Grid.grid) : string =
           let symbol = match orientation with
             | DownRight -> "╲"
             | UpRight -> "╱"
-            | _ -> failwith "Bumper can only be downright or upright in orientation."
+            | _ -> "?"
           in
           "<td class='bumper'>" ^ symbol ^ "</td>"
       | Entry { direction = Down } -> "<td class='entry'>o</td>"
@@ -29,62 +30,33 @@ let render_grid (grid : Grid.grid) : string =
   |> Array.to_list
   |> String.concat
 
-(* Generate new level and return it as a string *)
+(* Generate a new level and return it as a Dream HTML response *)
 let generate_level _ =
   let grid = Array.init 5 ~f:(fun x -> Array.init 5 ~f:(fun y -> { position = (x,y); cell_type = Empty })) in
-  grid.(0).(2) <- {position = (0, 2); cell_type = Entry {direction = Down}};
-  grid.(2).(2) <- {position = (2, 2); cell_type = Bumper {direction = Down; orientation = UpRight}};
-  (* let grid = Grid.create_test_grid () in *)
+  grid.(0).(2) <- { position = (0, 2); cell_type = Entry { direction = Down }};
+  grid.(2).(2) <- { position = (2, 2); cell_type = Bumper { direction = Down; orientation = UpRight }};
+  (* Generate grid HTML *)
   let grid_html = render_grid grid in
-  ("<div id='grid-container'>" ^ grid_html ^ "</div>"
-    ^ "<script>setTimeout(function() { document.getElementById('grid-container').innerHTML = ''; }, 3000);</script>")
+  Dream.html
+    ("<div id='grid-container'>" ^ grid_html ^ "</div>"
+     ^ "<script>setTimeout(function() { document.getElementById('grid-container').innerHTML = ''; }, 3000);</script>")
 
-(* Main page render *)
+(* Render the main page *)
 let render_main_page () =
-  Printf.sprintf
-    {|
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Pinball Panic</title>
-      <script>
-        function startNewLevel() {
-          fetch('/generate-level')
-            .then(response => response.text())
-            .then(function(html) {
-              document.getElementById('grid-container').innerHTML = html;
-            });
-        }
-      </script>
-    </head>
-    <body>
-      <h1>Welcome to Pinball Panic</h1>
-      <h2>Level %d</h2>
-      <div id='grid-container'></div>
-      <button onclick='startNewLevel()'>Start New Level</button>
-      <form action='/submit-answer' method='POST'>
-        <label for='answer'>Your Answer:</label>
-        <input type='text' id='answer' name='answer' required />
-        <button type='submit'>Submit</button>
-      </form>
-    </body>
-    </html>
-    |}
-    !current_level
+  "<!DOCTYPE html><html><head><title>Pinball Panic</title></head><body><h1>Test Page</h1></body></html>"
 
+(* Handle answer submission *)
 let submit_answer_handler request =
-  let body = Dream.body request in
+  (* let* body = Dream.body request in *)
   let is_correct = (* TODO: Replace this with logic to validate the answer *) true in
   if is_correct then (
-    (* Check if there is a next level *)
     if !current_level < 10 then (
       incr current_level;
-      Lwt.return (Dream.redirect request "/")
+      Dream.redirect request "/"
     ) else
-      Lwt.return (Dream.html "<h1>Congratulations! You completed all levels!</h1>")
+      Dream.html "<h1>Congratulations! You completed all levels!</h1>"
   ) else
-    (* Incorrect answer, tell user the correct answer *)
-    Lwt.return (Dream.html "<h1>Incorrect! The correct answer was ___</h1>")
+    Dream.html "<h1>Incorrect! Try again.</h1>"
 
 (* Dream Routes *)
 let () =
@@ -93,6 +65,6 @@ let () =
   @@ Dream.router
        [
          Dream.get "/" (fun _ -> Dream.html (render_main_page ()));
-         Dream.get "/generate-level" (fun _ -> Dream.html (generate_level ()));
+         Dream.get "/generate-level" (fun _ -> generate_level ());
          Dream.post "/submit-answer" submit_answer_handler;
        ]
