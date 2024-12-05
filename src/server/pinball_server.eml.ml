fabf85f8a59b5e4495b5462251f6842066dbef2f
Kenneth Elsman <98336240+Kenneth-E@users.noreply.github.com>
2024-12-04 21:21:28 -0500

    Submission flow works!

--- src/server/pinball_server.eml.ml
+++ src/server/pinball_server.eml.ml
@@ -36,30 +36,48 @@ let render_grid (grid : Grid.grid) : string =
 let generate_level _ =
   let (grid, _, end_pos, _) = Grid.generate_grid !current_level in
   (* Store answer for later *)
-  (current_answer.x) := fst end_pos;
-  (current_answer.y) := snd end_pos;
+  (current_answer.x) := snd end_pos;
+  (current_answer.y) := fst end_pos;
   (* Generate grid HTML *)
   let grid_html = render_grid grid in
   Lwt.return ("<div id='grid-container'>" ^ grid_html ^ "</div>")
 
 (* Render the main page, displays and hides grid when new level starts *)
 let render_main_page () =
-  In_channel.read_all "./server_templates/main_page.html"
+  try In_channel.read_all "./server_templates/main_page.html"
+  with Sys_error _ -> "<h1>Error: Main page template not found.</h1>"
 
-(* Handle answer submission *)
+(* Handle answer submission*)
 let submit_answer_handler request =
-  (* let* body = Dream.body request in *)
-  let is_correct = (* TODO: fetch user input answer *)
-    ((!(current_answer.x) = 4) && (!(current_answer.y) = 4))
-  in
-  if is_correct then (
-    if !current_level < 10 then (
-      incr current_level;
-      Dream.redirect request "/"
-    ) else
-      Dream.html "<h1>Congratulations! You completed all levels!</h1>"
-  ) else
-    Dream.html "<h1>Incorrect! Game Over!</h1>"
+  (* Log for debugging *)
+  Dream.log "Query parameters: x=%s, y=%s"
+    (Option.value ~default:"missing" (Dream.query request "x"))
+    (Option.value ~default:"missing" (Dream.query request "y"));
+  Dream.log "Correct answer: x=%i, y=%i"
+    (!(current_answer.x))
+    (!(current_answer.y));
+  (* Extract the "x" and "y" query parameters *)
+  match (Dream.query request "x", Dream.query request "y") with
+  | (Some x_value, Some y_value) -> (
+      try
+        (* Convert query values to integers *)
+        let x = int_of_string x_value in
+        let y = int_of_string y_value in
+        (* Validate the answer *)
+        let is_correct = x = !(current_answer.x) && y = !(current_answer.y) in
+        if is_correct then (
+          if !current_level < 13 then (
+            incr current_level;
+            Dream.redirect request "/"
+          ) else
+            Dream.html "<h1>Congratulations! You completed all levels!</h1>"
+        ) else
+          Dream.html "<h1>Incorrect! Game Over!</h1>"
+      with Failure _ ->
+        Dream.html "<h1>Invalid input! Please enter valid numbers for coordinates.</h1>")
+  | _ ->
+      (* Handle missing query parameters *)
+      Dream.html "<h1>Bad Request: Missing query parameters 'x' or 'y'.</h1>"
 
 (* Dream Routes *)
 let () =
@@ -69,5 +87,5 @@ let () =
        [
          Dream.get "/" (fun _ -> Dream.html (render_main_page ()));
          Dream.get "/generate-level" (fun _ -> let%lwt grid_html = generate_level () in Dream.html grid_html);
-         Dream.post "/submit-answer" submit_answer_handler;
+         Dream.get "/submit-answer" submit_answer_handler ;
        ]
