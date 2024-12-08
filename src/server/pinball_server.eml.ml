dbb36c6bac24f3fb2137b92988ce4f092d52838b
Kenneth Elsman <98336240+Kenneth-E@users.noreply.github.com>
2024-12-07 19:32:28 -0500

    minor changes and fixes

--- src/server/pinball_server.eml.ml
+++ src/server/pinball_server.eml.ml
@@ -4,12 +4,18 @@ open Grid_cell
 
 (* Game state information *)
 let current_level = ref 1
-type answer = {x : int ref; y : int ref}
-let current_answer = { x = ref 0; y = ref 0}
+type answer = {col : int ref; row : int ref}
+let current_answer = { col = ref 0; row = ref 0}
 
 (* Render the HTML grid based on a grid data structure *)
 let render_grid (grid : Grid.grid) : string =
   let num_cols = Array.length grid.(0) in
+  let num_rows = Array.length grid in
+
+  (* Check if a cell is part of the grid's perimeter *)
+  let is_on_perimeter i j =
+    i = 0 || i = num_rows - 1 || j = 0 || j = num_cols - 1
+  in
 
   (* Generate column headers *)
   let col_headers =
@@ -21,18 +27,19 @@ let render_grid (grid : Grid.grid) : string =
   let rows =
     Array.mapi grid ~f:(fun i row ->
       let row_cells =
-        Array.map row ~f:(fun cell ->
-          match to_string cell with 
+        Array.mapi row ~f:(fun j cell ->
+          let cell_class =
+            if is_on_perimeter i j then "outer-square" else "regular-square"
+          in
+          match to_string cell with
           | "Entry" -> "<td class='entry'>E</td>"
-          | "Exit" -> "<td class='exit'> </td>"
-          | "Empty" -> "<td class='empty'> </td>"
-          | "InBallPath" -> "<td class='empty'> </td>"
-          | "Bumper" -> "<td class='bumper'>" ^ (get_bumper_orientation_string cell.cell_type) ^ "</td>"
-          | "Tunnel" -> "<td class='tunnel'>" ^ (get_tunnel_orientation_string cell.cell_type) ^ "</td>"
-          | "Teleporter" -> "<td class='teleporter'>*</td>"
-          (* 
-          | "ActivatedBumper" *)
-          | _ -> failwith "Error: there shouldn't be any other grid cell type string within the grid other than the ones matched above."
+          | "Exit" -> "<td class='" ^ cell_class ^ "'></td>"
+          | "Empty" -> "<td class='" ^ cell_class ^ "'></td>"
+          | "InBallPath" -> "<td class='path'></td>"
+          | "Bumper" -> "<td class='bumper dynamic'>" ^ (get_bumper_orientation_string cell.cell_type) ^ "</td>"
+          | "Tunnel" -> "<td class='tunnel dynamic'>" ^ (get_tunnel_orientation_string cell.cell_type) ^ "</td>"
+          | "Teleporter" -> "<td class='teleporter dynamic'>*</td>"
+          | _ -> failwith "Unexpected grid cell type."
         )
         |> Array.to_list
         |> String.concat
@@ -48,13 +55,19 @@ let render_grid (grid : Grid.grid) : string =
 
 (* Generate a new level and return it as a Dream HTML response *)
 let generate_level _ =
+  (* Check if grid is revealed *)
+  (* let revealed = 
+    match Dream.query request "revealed" with
+    | Some "true" -> true
+    | _ -> false
+  in *)
   let (grid, _, end_pos, _) = Grid.generate_grid !current_level in
   (* Store answer for later *)
-  (current_answer.x) := snd end_pos;
-  (current_answer.y) := fst end_pos;
+  (current_answer.col) := snd end_pos;
+  (current_answer.row) := fst end_pos;
   (* Generate grid HTML *)
   let grid_html = render_grid grid in
-  Lwt.return ("<div id='grid-container'>" ^ grid_html ^ "</div>")
+  Dream.html ("<div id='grid-container'>" ^ grid_html ^ "</div>")
 
 (* Render the main page, displays and hides grid when new level starts *)
 let render_main_page () =
@@ -62,25 +75,24 @@ let render_main_page () =
   let current_level_str = string_of_int !current_level in
   String.substr_replace_all html_template ~pattern:"{{current_level}}" ~with_:current_level_str
 
-
 (* Handle answer submission*)
 let submit_answer_handler request =
   (* Log for debugging *)
-  Dream.log "Query parameters: x=%s, y=%s"
-    (Option.value ~default:"missing" (Dream.query request "x"))
-    (Option.value ~default:"missing" (Dream.query request "y"));
-  Dream.log "Correct answer: x=%i, y=%i"
-    (!(current_answer.x))
-    (!(current_answer.y));
-  (* Extract the "x" and "y" query parameters *)
-  match (Dream.query request "x", Dream.query request "y") with
-  | (Some x_value, Some y_value) -> (
+  Dream.log "Query parameters: col=%s, row=%s"
+    (Option.value ~default:"missing" (Dream.query request "col"))
+    (Option.value ~default:"missing" (Dream.query request "row"));
+  Dream.log "Correct answer: col=%i, row=%i"
+    (!(current_answer.col))
+    (!(current_answer.row));
+  (* Extract the "col" and "row" query parameters *)
+  match (Dream.query request "col", Dream.query request "row") with
+  | (Some col_value, Some row_value) -> (
       try
         (* Convert query values to integers *)
-        let x = int_of_string x_value in
-        let y = int_of_string y_value in
+        let col = int_of_string col_value in
+        let row = int_of_string row_value in
         (* Validate the answer *)
-        let is_correct = x = !(current_answer.x) && y = !(current_answer.y) in
+        let is_correct = col = !(current_answer.col) && row = !(current_answer.row) in
         if is_correct then (
           if !current_level < 13 then (
             incr current_level;
@@ -102,6 +114,6 @@ let () =
   @@ Dream.router
        [
          Dream.get "/" (fun _ -> Dream.html (render_main_page ()));
-         Dream.get "/generate-level" (fun _ -> let%lwt grid_html = generate_level () in Dream.html grid_html);
-         Dream.get "/submit-answer" submit_answer_handler ;
+         Dream.get "/generate-level" generate_level;
+         Dream.get "/submit-answer" submit_answer_handler;
        ]
